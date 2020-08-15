8d8a9b676bc0f9e8eeb5dedae4577f428c94f07d
Humberto Anjos <h.anjos@gmail.com>
2020-08-15 16:54:50 -0300

    Instalando Redis para atuar como cache.

--- main.js
+++ main.js
@@ -1,11 +1,66 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
+const redis = require('redis');
+
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT || 8080;
+const CACHE_TIMEOUT = process.env.CACHE_TIMEOUT || 3600000; // milissegundos
+
+const cache = redis.createClient(
+  process.env.REDIS_PORT,
+  process.env.REDIS_HOST,
+  {
+    auth_pass: process.env.REDIS_ACCESS_KEY,
+    tls: {
+      servername: process.env.REDIS_HOST
+    }
+  });
+
+// se retornar nil, ou não há valor no cache ou o valor é muito antigo
+function checkCache(cache, key) {
+  return new Promise((resolve, reject) => {
+    cache.hgetall(key, function (err, val) {
+      if(err) {
+        reject(err);
+        return;
+      }
+
+      const now = Date.now();
+      if(!val || val.timestamp <= now - CACHE_TIMEOUT) {
+        resolve(nil);
+      } else {
+        resolve(val.value);
+      }
+    });
+  });
+}
+
+function updateCache(cache, key, value) {
+  return new Promise((resolve, reject) => {
+    cache.hmset(key, 
+      'timestamp', Date.now(), 
+      'value', value,
+      (err, v) => {
+        if(err) { reject(err); }
+        else { resolve(value); }
+      });
+  });
+}
+
+function bodyOf(request) {
+  return new Promise((resolve, reject) => {
+    let body = [];
+
+    request.on('error', (err) => { reject(err); })
+      .on('data', (chunk) => { body.push(chunk); })
+      .on('end', () => { resolve(Buffer.concat(body).toString()); });
+  });
+}
 
 function callBackendWith(options) {
   return new Promise((resolve, reject) => {
     const req = http.request(options, (res) => {
-      console.log("Chamando o backend");
+      console.log("Chamando o backend com o seguinte payload: '%s'", options.body);
 
       let result = [];
 
@@ -21,19 +76,29 @@ function callBackendWith(options) {
 }
 
 async function getDetailsOf(stock) {
-  return await callBackendWith({
-      host: 'fundamentus.com.br',
-      path: '/detalhes.php?papel=' + stock,
-      method: 'GET',
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-        'Cookie': "PHPSESSID=9880b6d9d5575ed4a635737b1977fe87; __utma=138951332.1866296538.1534982752.1534982752.1534982752.1; __utmc=138951332; __utmz=138951332.1534982752.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=138951332.9.10.1534982752"
-      }
-    });
+  let cachedValue = await checkCache(cache, stock);
+  if(cachedValue) {
+    return cachedValue;
+  }
+
+  return updateCache(cache, stock, await callBackendWith({
+    host: 'fundamentus.com.br',
+    path: '/detalhes.php?papel=' + stock,
+    method: 'GET',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+      'Cookie': "PHPSESSID=9880b6d9d5575ed4a635737b1977fe87; __utma=138951332.1866296538.1534982752.1534982752.1534982752.1; __utmc=138951332; __utmz=138951332.1534982752.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=138951332.9.10.1534982752"
+    }
+  }));
 }
 
 async function redirectToBackend(body) {
-  return await callBackendWith({
+  let cachedValue = await checkCache(cache, body);
+  if(cachedValue) {
+    return cachedValue;
+  }
+
+  return updateCache(cache, body, await callBackendWith({
       host: 'fundamentus.com.br',
       path: '/resultado.php',
       method: 'POST',
@@ -42,21 +107,8 @@ async function redirectToBackend(body) {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
       },
       body: body
-    });
+    }));
 }
-
-function bodyOf(request) {
-  return new Promise((resolve, reject) => {
-    let body = [];
-
-    request.on('error', (err) => { reject(err); })
-      .on('data', (chunk) => { body.push(chunk); })
-      .on('end', () => { resolve(Buffer.concat(body).toString()); });
-  });
-}
-
-const API_KEY = process.env.API_KEY;
-const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(async (request, response) => {
   const { method, headers } = request;
