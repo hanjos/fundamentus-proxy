1cd342e02658c9ed1bc2924602a891831ebdd9f6
Humberto Anjos <h.anjos@gmail.com>
2020-08-15 18:14:10 -0300

    Testando um POST com body conhecido

--- main.js
+++ main.js
@@ -144,7 +144,7 @@ async function redirectToBackend(body, clearCache) {
         'Content-Type': 'application/x-www-form-urlencoded',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
       },
-      body: body
+      body: 'pl_min=0.0&pl_max=12.0&roic_min=0.12&negociada=ON&ordem=1.0'//body
     }));
 }
 
